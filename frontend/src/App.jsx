e846572a52e652d9ae6da7c730af4ee2e8347e03
MAHEE-JAISWAL <=>
2025-02-17 13:02:35 +0530

    Added PrivateRoute.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,18 @@ import Home from './pages/Home';
 import Demo from './pages/Demo';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsAndConditions from './pages/TermsAndConditions';
-
+import PrivateRoute from './PrivateRoute';
 
 const App = () => {
   return (
-
     <Routes>
       <Route path="/" element={<SignupForm />} />
       <Route path="/login" element={<LoginForm />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/demo" element={<Demo />} />
+      <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+      <Route path="/demo" element={<PrivateRoute><Demo /></PrivateRoute>} />
       <Route path="/terms" element={<TermsAndConditions />} />
       <Route path="/privacy" element={<PrivacyPolicy />} />
     </Routes>
-
   );
 };
 
