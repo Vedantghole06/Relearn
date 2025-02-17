5eb0cf90dc92f58be4d104640b24c24900df78e7
Vedantghole06 <Vedantghole06@gmail.com>
2025-02-17 12:49:09 +0530

    modified index.js in frontend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Use CORS middleware
 app.use(cors({
-    origin: 'http://localhost:5174', // Update this to match your frontend URL
+    origin: 'http://localhost:5173', // Update this to match your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies to be sent with requests
 }));
